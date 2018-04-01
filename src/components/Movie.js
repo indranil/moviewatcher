993d7ad713b7067ccddc81ebfc0ac67ca2ed0006
Indranil Dasgupta <indranild@gmail.com>
2018-04-02 00:54:49 +0200

    spiffied up code and removed results with no posters 🚀

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -8,19 +8,28 @@ class Movie extends Component {
       return this.props.onMovieRemove(this.props.movie.id);
     }
   }
+  
   render() {
-    return (
-      <li><a href={`https://www.themoviedb.org/movie/${this.props.movie.id}`} target="_blank">
-        <div className="img-container">
-          <img src={`http://image.tmdb.org/t/p/w185${this.props.movie.poster_path}`} alt={this.props.movie.title} />
-        </div>
-        <h3>{this.props.movie.title}</h3></a>
-        <p>({this.props.movie.release_date})</p>
-        <button className={this.props.type} onClick={this.buttonType}>{this.props.type}</button>
-      </li>
-    )
+    const {
+      movie,
+      type
+    } = this.props;
+
+    return (movie.poster_path === null)
+    ?
+      null
+    :
+      (
+        <li>
+          <a href={`https://www.themoviedb.org/movie/${movie.id}`} target="_blank">
+            <img src={`http://image.tmdb.org/t/p/w185${movie.poster_path}`} alt={movie.title} />
+            <h3>{movie.title}</h3>
+          </a>
+          <p>({movie.release_date})</p>
+          <button className={type} onClick={this.buttonType}>{type}</button>
+        </li>
+      );
   }
 }
-
 
 export default Movie;
